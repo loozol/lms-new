08e3c54f552d1d0d2a20d23b37017c548bbf155d
Vera <loozol59@gmail.com>
2019-12-25 04:53:09 +0300

    Initial commit

---
+++ src/Fine.java
@@ -0,0 +1,22 @@
+public class Fine {
+
+    int fineID;
+    int resourceID;
+    int userID;
+    int fine;
+
+    //Constructor for Fine class...
+    Fine(int _resID, int userID, int _fine) {
+        fineID = Library.nextFineID;
+        Library.nextFineID++;
+        this.resourceID = _resID;
+        this.fine = _fine;
+        this.userID = userID;
+    }
+
+    //to update fine.
+    void updateFine(int fine) {
+        this.fine = fine;
+    }
+
+}
